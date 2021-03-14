21ee2fcb849cc9b857bb1f77307a0e88bc868a21
Amanda <amandaben21@aol.com>
2021-03-13 19:00:22 -0500

    done

--- db/migrate/20210313231937_create_appointments.rb
+++ db/migrate/20210313231937_create_appointments.rb
@@ -5,7 +5,7 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
       t.integer :patient_id
       t.integer :doctor_id
 
-      t.timestamps
+      t.timestamps null:false
     end
   end
 end
